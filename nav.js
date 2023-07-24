6f36a52439049b213903566b312acff56c91653a
Duskope <noahpotter33@gmail.com>
2023-07-24 17:54:42 -0400

    changes to mobile nav functionality

--- nav.js
+++ nav.js
@@ -1,7 +1,22 @@
 let icon = document.querySelector(".nav-btn");
 let mobileNav = document.getElementById("dropdown-nav");
+let dropdownBtn = document.querySelectorAll(".dropdown-link");
+let menu = document.getElementById('menu')
+let about = document.getElementById('about')
 
 icon.addEventListener("click", () => {
   icon.classList.toggle("clicked");
   mobileNav.classList.toggle("active");
 });
+dropdownBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        icon.classList.toggle('clicked')
+        mobileNav.classList.toggle('active')
+    })
+});
+function scrollMenu() {
+    menu.scrollIntoView
+}
+function scrollAbout() {
+    about.scrollIntoView
+}
